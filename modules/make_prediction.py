99a292c4b65b9578cb9338b993dd38e609362434
dzverkov <dzverkov@gmail.com>
2020-04-11 13:28:35 +0300

    cw1

--- modules/make_prediction.py
+++ modules/make_prediction.py
@@ -3,16 +3,26 @@ import pandas as pd
 import settings
 from sklearn.preprocessing import MinMaxScaler
 
+from utils.utils import get_logger, time_format
+
 
 def main():
+    logger = get_logger('make_prediction')
+
     with open('{}/baseline_xgb.pcl'.format(settings.MODEL_PATH), 'rb') as f:
         model = pickle.load(f)
 
     X = pd.read_csv('{}dataset_test.csv'.format(settings.DATASET_PATH), sep=';')
-    #X_mm = MinMaxScaler().fit_transform(X)
+    X_mm = X.drop(['user_id'], axis=1)
+    X_mm = MinMaxScaler().fit_transform(X_mm)
 
-    predict_test = model.predict(X)
-    print(predict_test.head())
+    predict_test = model.predict(X_mm)
+    X_pred = pd.DataFrame(X['user_id'])
+    X_pred['is_churned'] = predict_test
+
+    X_pred.to_csv('{}dataset_pred.csv'.format(settings.PREDICTION_PATH), sep=';', index=False)
+
+    logger.info('Prediction is successfully saved to {}'.format(settings.PREDICTION_PATH))
 
 
 if __name__ == "__main__":
--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
-
 INTER_1 = (1, 7)
 INTER_2 = (8, 14)
 INTER_3 = (15, 21)
 INTER_4 = (22, 28)
 INTER_LIST = [INTER_1, INTER_2, INTER_3, INTER_4]
 
-RAW_DATA_PATH='./../data/source/'
-DATASET_PATH='./../data/dataset/'
-MODEL_PATH='./../data/model/'
+RAW_DATA_PATH = './../data/source/'
+DATASET_PATH = './../data/dataset/'
+MODEL_PATH = './../data/model/'
+PREDICTION_PATH = './../data/prediction/'
